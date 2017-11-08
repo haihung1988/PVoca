8828e226289f2a0ec6c6f785e4a8398887e168f0
Hung Hoang Hai (Lazada Group) <hung.hoanghai@lazada.com>
2017-11-08 10:37:50 +0700

    Init project

---
+++ PVoca/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PVoca
+//
+//  Created by Hung Hoang Hai (Lazada Group) on 11/8/17.
+//  Copyright © 2017 hhh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
